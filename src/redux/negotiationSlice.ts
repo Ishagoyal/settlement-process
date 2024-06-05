5d870aa055fb7d85f2963a62026254df1bd35228
ishagoyal <iamishagoyal@gmail.com>
2024-06-05 16:27:29 +0530

    Implement redux

---
+++ src/redux/negotiationSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface NegotiationState {
+  amount: number | null;
+  response: {
+    agreed: boolean;
+    message?: string;
+  };
+}
+
+const initialState: NegotiationState = {
+  amount: null,
+  response: {
+    agreed: false,
+  },
+};
+
+export const negotiationSlice = createSlice({
+  name: "negotiation",
+  initialState,
+  reducers: {
+    submitAmount: (state, action: PayloadAction<number>) => {
+      state.amount = action.payload;
+    },
+    resetAmount: (state) => {
+      state.amount = null;
+    },
+    setResponse: (
+      state,
+      action: PayloadAction<{ agreed: boolean; message?: string }>
+    ) => {
+      state.response = action.payload;
+    },
+  },
+});
+
+export const { submitAmount, resetAmount, setResponse } =
+  negotiationSlice.actions;
+
+export default negotiationSlice.reducer;
